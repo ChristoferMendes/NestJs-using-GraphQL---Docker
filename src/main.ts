43954699cace31f2fdff003f77ebc5e3ff293cfa
ChristoferMendes <christoferluizdsm@gmail.com>
2022-08-05 14:54:15 -0300

    Added some console infos

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.API_PORT || 9000);
+  console.info('Listening on PORT: ' + process.env.API_PORT);
+  console.log(
+    'Type http://localhost:' +
+      process.env.API_PORT +
+      '/graphql to open the playground',
+  );
 }
 bootstrap();
